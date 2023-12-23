7af3a498c1c3ea89ec6cd3894a7b5d6a5734357a
FilipSzkarlat <filip.szkarlat@gmail.com>
2023-12-23 13:38:20 +0100

    Using ngModel to bind values to form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   items: WishItem[] = [
-    new WishItem('Learn Angular'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Find grass that cuts itself'),
+    // new WishItem('Learn Angular'),
+    // new WishItem('Get Coffee', true),
+    // new WishItem('Find grass that cuts itself'),
   ];
+
+  newWishText = '';
+
   title = 'wishlist';
+
+  addNewWish() {
+    // todo: add wish to items array
+    this.items.push(new WishItem(this.newWishText));
+    // clear the textbox
+    this.newWishText = '';
+  }
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
