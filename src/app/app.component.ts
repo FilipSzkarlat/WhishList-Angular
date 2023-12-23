7dfd2acef143da3aa41a3f956b8e27db7472a641
FilipSzkarlat <filip.szkarlat@gmail.com>
2023-12-23 17:33:44 +0100

    Listening for changes to ngModel values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,20 +13,34 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   items: WishItem[] = [
-    // new WishItem('Learn Angular'),
-    // new WishItem('Get Coffee', true),
-    // new WishItem('Find grass that cuts itself'),
+    new WishItem('Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself'),
   ];
+
+  listFilter: String = '0';
 
   newWishText = '';
 
   title = 'wishlist';
+
+  visibileItems: WishItem[] = this.items;
 
   addNewWish() {
     // todo: add wish to items array
     this.items.push(new WishItem(this.newWishText));
     // clear the textbox
     this.newWishText = '';
+  }
+
+  filterChanged(value: any) {
+    if (value === '0') {
+      this.visibileItems = this.items;
+    } else if (value === '1') {
+      this.visibileItems = this.items.filter((item) => !item.isComplete);
+    } else {
+      this.visibileItems = this.items.filter((item) => item.isComplete);
+    }
   }
 
   toggleItem(item: WishItem) {
